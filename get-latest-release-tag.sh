9cdd910e7c3eaf7afaf96aa1be24c8bc48a12e2d
Jeremy Lin <jeremy.lin@gmail.com>
2021-01-14 20:37:36 -0800

    Fix parsing of JSON responses from the GitHub releases API
    The JSON response is usually (but for some reason, not always)
    pretty-printed, so a simple line-oriented grep doesn't always work.

--- get-latest-release-tag.sh
+++ get-latest-release-tag.sh
@@ -14,16 +14,26 @@ fi
 
 owner="$1"
 repo="$2"
+latest_release_url="https://api.github.com/repos/${owner}/${repo}/releases/latest"
 
-# The API returns an output like
+# Given a JSON input on stdin, extract the string value associated with the
+# specified key. This avoids an extra dependency on a tool like `jq`.
+extract() {
+    local key="$1"
+    # Extract "<key>":"<val>" (assumes key/val won't contain double quotes).
+    # The colon may have whitespace on either side.
+    grep -o "\"${key}\"[[:space:]]*:[[:space:]]*\"[^\"]\+\"" |
+    # Extract just <val> by deleting the last '"', and then greedily deleting
+    # everything up to '"'.
+    sed -e 's/"$//' -e 's/.*"//'
+}
+
+# The API returns a JSON output like
 #
 # [...snip...]
 #   "tag_name": "1.2.3",
 # [...snip...]
 #
-# To extract "1.2.3", we delete (1) everything up to and including ':',
-# (2) whitespace and '"' from the front, and (3) ',' and '"' from the back.
+# This output is usually (but for some reason, not always) pretty-printed.
 #
-curl -sSL https://api.github.com/repos/"${owner}"/"${repo}"/releases/latest \
-    | grep 'tag_name' \
-    | sed -e 's/[^:]\+://' -e 's/^[ "]\+//' -e 's/[,"]\+$//'
+curl -fsSL "${latest_release_url}" | extract 'tag_name'
